19a90a31d37b4bd0597b02c47da08d8dbb4afaaa
ropeJIbI4 <pogi199411@gmail.com>
2024-05-26 02:53:47 +0300

    first

---
+++ vue_7/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import Index from './components/Index.vue';
+import Blog from './components/Blog.vue';
+import BlogDetails from './components/BlogDetails.vue';
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import ProductDetails from './components/ProductDetails.vue';
+import OurProject from './components/OurProject.vue';
+import ProjectDetails from './components/ProjectDetails.vue';
+import index from './store/index'
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+    <Header />
+    <!-- <Index/> -->
+    <!-- <Blog/> -->
+    <!-- <BlogDetails /> -->
+    <!-- <ProductDetails/> -->
+    <!-- <OurProject/> -->
+    <ProjectDetails/>
+    <Footer/>
+  </main>
+</template>
+
+
+<style>
+
+</style>
+
+
---
+++ vue_7/src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="header center">
+        <div class="header__nav">
+            <div>
+                <img class="header__logo" src="/public/assets/Logo_header.png" alt="">
+            </div>
+            <div class="header__link">
+                <a href="#">Home</a>
+                <a href="#">Project</a>
+                <a href="#">Blog</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+a {
+    text-decoration: none;
+    color: black;
+}
+
+.center {
+    padding-left: calc(50% - 560px);
+    padding-right: calc(50% - 560px);
+}
+
+.header__nav {
+    padding-right: 10px;
+    padding-left: 10px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.header__link {
+    display: flex;
+    padding-top: 15px;
+    gap: 15px;
+}
+
+.header__logo {
+    width: 176px;
+}
+</style>
